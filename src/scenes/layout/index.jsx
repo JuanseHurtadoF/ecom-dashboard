b4c0d89fd586d5e279e33179044541d702db4393
JuanseHurtadoF <juansebastianhurtadof@gmail.com>
2023-03-30 14:37:58 -0500

    remove search bar and fix sidebar bug on resize

--- src/scenes/layout/index.jsx
+++ src/scenes/layout/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Box, useMediaQuery } from "@mui/material";
 import { Outlet } from "react-router-dom";
 import { useSelector } from "react-redux";
@@ -11,6 +11,24 @@ const Layout = () => {
   const [isSidebarOpen, setIsSidebarOpen] = useState(true);
   const userId = useSelector((state) => state.global.userId);
   const { data } = useGetUserQuery(userId);
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth < 800) {
+        setIsSidebarOpen(false);
+      } else {
+        setIsSidebarOpen(true);
+      }
+    };
+
+    handleResize(); // Call the handler once on initial render
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      // Clean up the event listener on component unmount
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
 
   return (
     <Box display={isNonMobile ? "flex" : "block"} width="100%" height="100%">
